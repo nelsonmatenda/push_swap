7a192dc3c4611f56a1ead9ae604a0aad54449125
nelsonmatenda <matendafigueiredo@hotmail.com>
2024-07-18 02:41:21 +0100

    implementando a estrutura pilha.

--- includes/list.h
+++ includes/list.h
@@ -5,11 +5,22 @@
 #include <unistd.h>
 #include <stdio.h> // TODO: dev
 
-typedef struct s_list
+typedef struct s_node
 {
 	int	value;
-	struct s_list	*next;
+	struct s_node	*next;
+}	t_node;
+
+typedef struct s_list
+{
+	t_node	*begin;
+	t_node	*end;
+	size_t	size;
 }	t_list;
 
+t_node	*create_node(int value);
+t_list	*creat_list(void);
+void	push(t_list *lst, int value);
+t_node	*pop(t_list *lst);
 
 #endif
--- src/list.c
+++ src/list.c
@@ -1,1 +1,46 @@
-#include "list.h"
+#include "../includes/list.h"
+
+t_node	*create_node(int value)
+{
+	t_node	*node;
+
+	node = (t_node *)malloc(sizeof(t_node));
+	node->value = value;
+	node->next = NULL;
+	return (node);
+}
+
+t_list	*creat_list(void)
+{
+	t_list	*lst;
+
+	lst = (t_list *)malloc(sizeof(t_list));
+	lst->begin = NULL;
+	lst->end = NULL;
+	lst->size = 0;
+	return (lst);
+}
+
+void	push(t_list *lst, int value)
+{
+	t_node	*new;
+	
+	new = create_node(value);
+	if (lst->size == 0)
+		lst->end = new;
+	new->next = lst->begin;
+	lst->begin = new;
+	lst->size++;
+}
+
+t_node	*pop(t_list *lst)
+{
+	t_node *node;
+
+	if (lst->size == 0)
+		return (NULL);
+	node = lst;
+	lst = lst->begin->next;
+	lst->size--;
+	return (node);
+}
